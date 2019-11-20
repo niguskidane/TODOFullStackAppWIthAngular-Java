07792e2ef9eed805a22785a95cd3af1e45b8038e
King <nigus.kidane@gmail.com>
2019-11-20 11:30:14 -0700

    Project added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  //template:'<h2>{{title}}</h2>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'TODO';
+  message='Angular Course';
 }
